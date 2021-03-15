16cda07a3c9bb44b954bfacf92dd18f1c755eee6
r3d__@hotmail.com <r3d>
2021-03-15 14:37:31 -0400

    added requested functionality

--- BFGetNames/Program.cs
+++ BFGetNames/Program.cs
@@ -1,6 +1,8 @@
 ﻿#nullable enable
 
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
 
 namespace BFGetNames
 {
@@ -8,25 +10,65 @@ namespace BFGetNames
 	{
 		public static void Main(string[] args)
 		{
-			Console.WriteLine("Enter some text to search for:");
-			string? filter = Console.ReadLine();
-			if (filter == null)
-				return;
+			string? filter;
+			while (true)
+			{
+				Console.WriteLine("Enter some text to search for:");
+				filter = Console.ReadLine();
+				if (!String.IsNullOrEmpty(filter))
+				{
+					break;
+				}
+			}
 
 			string[] names = GetNames(filter);
-			foreach (string name in names)
-				Console.WriteLine(name);
+			Console.WriteLine($"{names.Length} results were found for [ {filter} ]");
+
+			if (names.Length > 0)
+			{
+				foreach (string name in names)
+				{
+					Console.WriteLine(name);
+				}
+			}
 
 			Console.WriteLine("Press any key to exit...");
 			Console.ReadKey();
+
 		}
+
 
 		public static string[] GetNames(string filter)
 		{
-			string jsonData = JsonData.GetJsonData();
+
+			string json = JsonData.GetJsonData();
+			List<string> names = new List<string>();
+			JArray jArray = JArray.Parse(json);
 
 			//todo: take this json data and return an array of names after filtering by whatever the user types in.
 			//todo: if you have any questions please email me at jtackabury@binaryfortress.com
+			try
+			{
+				foreach (JObject item in jArray)
+				{
+					string countryName = item.GetValue("name").ToString();
+					string countryCode = item.GetValue("code").ToString();
+
+					if (!String.IsNullOrEmpty(countryName) && !String.IsNullOrEmpty(countryCode))
+					{
+						if (countryName.Contains(filter, StringComparison.OrdinalIgnoreCase) || countryCode.Contains(filter, StringComparison.OrdinalIgnoreCase))
+						{
+							names.Add(countryName);
+						}
+					}
+				}
+			}
+			catch
+			{
+				throw;
+			}
+
+			return names.ToArray();
 		}
 	}
 }
